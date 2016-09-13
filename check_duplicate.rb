c558bd7398cdbfaf6e7e05ab5ceeb80146f5734a
Lucas, Kenny <klucas@gannett.com>
2016-09-13 13:09:32 -0400

    updated

--- check_duplicate.rb
+++ check_duplicate.rb
@@ -5,16 +5,16 @@ def check_duplicate(duplicate_array, duplicate_count, testname)
 
 	is_duplicate = false
 
-	puts "Duplicate array count #{duplicate_count}"
+	#puts "Duplicate array count #{duplicate_count}"
 
 	for idx in 0 .. duplicate_count
 		unless is_duplicate
-			puts "idx=#{idx}, Compare *#{duplicate_array[idx]}*"
-			puts "*#{testname}*"
+			#puts "idx=#{idx}, Compare *#{duplicate_array[idx]}*"
+			#puts "*#{testname}*"
 			
 			if duplicate_array[idx] == testname
 				is_duplicate = true
-				puts "DUPLICATE WAS FOUND"
+				#puts "DUPLICATE WAS FOUND"
 			end
 		end
 	end
